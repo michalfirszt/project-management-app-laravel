5c9eb201a1360a9ffd53660717f4cd1b089f5105
michalfirszt <michalfiszu@gmail.com>
2019-02-08 10:49:13 +0100

    project-task-relationship

--- database/migrations/2019_02_08_080237_create_tasks_table.php
+++ database/migrations/2019_02_08_080237_create_tasks_table.php
@@ -15,8 +15,13 @@ class CreateTasksTable extends Migration
     {
         Schema::create('tasks', function (Blueprint $table) {
             $table->increments('id');
+            $table->unsignedInteger('project_id');
             $table->string('title');
             $table->timestamps();
+
+            $table->foreign('project_id')
+                    ->references('id')->on('projects')
+                    ->onDelete('cascade');
         });
     }
 
